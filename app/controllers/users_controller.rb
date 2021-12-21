b405075527588f49bc19cd102efdc7b3723eb3fd
hiroyukiuchiyama <uchiyamaworld@gmail.com>
2021-12-21 03:07:25 +0000

    users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+   before_action :ensure_correct_user, only: [:edit,:update,:destroy]
+
+    def show
+        @user = User.find(params[:id])
+        @hobbys = @user.hobbies
+        @hobby = Hobby.new
+        @hobby.build_hobby_image
+    end
+
+    def index
+        @users = User.all
+        @hobby = Hobby.new
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def updae
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+         redirect_to user_path(@user), notice: "You have updated user successfully."
+        else
+         render 'edit'
+        end
+    end
 end
