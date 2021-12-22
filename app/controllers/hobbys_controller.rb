e7ff6f35cef7649d8e8fade4cddae032aebff60b
hiroyukiuchiyama <uchiyamaworld@gmail.com>
2021-12-22 06:23:47 +0000

    controller画像解析用

--- app/controllers/hobbys_controller.rb
+++ app/controllers/hobbys_controller.rb
@@ -1,27 +1,22 @@
 class HobbysController < ApplicationController
     before_action :ensure_correct_user, only:[:edit, :update,:destroy]
-
+    
      def show
          @hobby = Hobby.find(params[:id])
          @user = @hobby.user
          @hobbys = Hobby.new
          @hobbys.build_hobby_image
      end
-
-     def index
+     
+     def index 
          @hobby = Hobby.new
          @hobbys = Hobby.all
          @hobby.build_hobby_image
-
+         
      end
-
+     
      def create
-        @hobby = Hobby.new(hobby_params)
-         tags = Vision.get_image_data(hobby.image)
-         tags.each do |tag|
-         @hobby.tags.create(name: tag)
-         end
-        @hobby.score = Language.get_data(hobby_params[:body])
+        @hobby.score = Language.get_data(hobby_params[:body])  #この行を追加
         @hobby.user_id = current_user.id
       if @hobby.save
         redirect_to hobby_path(@hobby), notice: "You have created hobby successfully."
@@ -30,11 +25,11 @@ class HobbysController < ApplicationController
         render 'index'
       end
      end
-
+     
      def edit
         @hobby = Hobby.find(params[:id])
      end
-
+     
      def update
       @hobby = Hobby.find(params[:id])
       if @hobby.update(hobby_params)
@@ -43,19 +38,19 @@ class HobbysController < ApplicationController
           render 'edit'
       end
      end
-
+     
      def destroy
          @hobby = Hobby.find(params[:id])
          @hobby.destroy
-         redirect_to hobbys_path
+         redirect_to hobbys_path 
      end
-
-
+    
+    
     private
-
+    
     def hobby_params
         params.require(:hobby).permit(:title, :body,:rate, hobby_image_attributes: [:image])
-
+        
     end
 
   def ensure_correct_user
